871cc475fba49b564d68883b43dbc43654783de2
ohchanju <ohchanju3@naver.com>
2023-06-19 03:07:59 +0900

    아직.. 등호가 안됨 ..

--- calculator/app.js
+++ calculator/app.js
@@ -1,11 +1,55 @@
-const buttons = document.querySelectorAll('#number');
+const numberButtons = document.querySelectorAll('#number');
+const operatorButtons=document.querySelectorAll('.operator')
+const dotButton = document.querySelector('.dot');
+const equalButton = document.querySelector('.equal');
+const resetButton = document.querySelector('#reset');
+const resultElement = document.getElementById('result');
 
+let currentResult = ''; // 현재 결과 값
 
-buttons.forEach(button => {
+// 숫자버튼
+numberButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const result = document.getElementById('result');
-        const value = button.textContent;
+    const value = button.textContent;
+    currentResult += value;
+    resultElement.value = currentResult;
+  });
+});
 
-      result.value += value;
-    });
+// 연산자버튼
+operatorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const operator = button.textContent;
+    currentResult += operator;
+    resultElement.value = currentResult;
+  });
+});
+
+// 소수점 버튼 클릭 시
+dotButton.addEventListener('click', () => {
+  if (!currentResult.includes('.')) {
+    currentResult += '.';
+    resultElement.value = currentResult;
+  }
+});
+
+// 리셋 버튼 클릭 시
+resetButton.addEventListener('click', () => {
+  currentResult = '';
+  resultElement.value = '';
+});
+
+equalButton.addEventListener('click', () => {
+  const expression = currentResult;
+  let result;
+
+  try {
+    result = eval(expression);
+  } catch (error) {
+    console.log('계산 오류:', error);
+    result = '오류';
+  }
+
+  resultElement.value = result;
+  currentResult = result.toString();
 });
